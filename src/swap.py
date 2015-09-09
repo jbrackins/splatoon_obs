ecd4cbf21781d612abd99d7d92d72bfc8bf9994c
jbrackins <julian.brackins@mines.sdsmt.edu>
2015-09-08 18:44:52 -0600

    Image swapping works! Probably buggy because twitter is being weird about showing all Turf Wars updates. This currently assumes the MOST RECENT tweet is a ranked update

--- src/swap.py
+++ src/swap.py
@@ -1,5 +1,6 @@
 import path
-#import twitter
+import twitter
+import os
 
 class swapper:
     def __init__(self, debug=False):
@@ -9,10 +10,30 @@ class swapper:
         self.game = 'none'
         self.mapA = 'none'
         self.mapB = 'none'
+        self.mapAPath = 'none'
+        self.mapBPath = 'none'
+
+    def run(self):
+        self.checkTwitter()
+        self.prepMap("A", self.mapA)
+        self.prepMap("B", self.mapB)
+        self.swapMaps(self.mapAPath, self.mapBPath)
+        self.setText(self.game)
+        return
 
     def checkTwitter(self):
         #set lobby, game, mapA, mapB
+        self.lobby, self.game, self.mapA, self.mapB = twitter.getRecent()
+        #print "FOUND:", self.lobby, self.game, self.mapA, self.mapB
         return
+
+    def prepMap(self, ab, map):
+        words = map.split()
+        filename = words[0].lower() + words[1] + ".png"
+        if "A" not in ab:
+            self.mapBPath = filename
+        else:
+            self.mapAPath = filename
 
     def swapMaps(self, mapA, mapB):
         #swap the map files so they're current
@@ -21,12 +42,24 @@ class swapper:
         return
 
     def setMap(self, ab, map):
+        changeMap = "a.png"        
+        if "A" not in ab:
+            changeMap = "b.png"
+        #cp <map.png> <aorb.png>
+        cmd = "cp " + self.PATH.MAP_DIR + "/" + map + " "
+        cmd = cmd + self.PATH.CURRENT_DIR + "/" + changeMap
+        os.system(cmd)
         #setting an individual map file
+        
         return
 
     def setText(self, txt):
         #overwrite the current.txt file with self.game
+        f = open( self.PATH.CURRENT_DIR + "/" + self.PATH.TXT , 'w')
+        f.write(txt)
+        f.close
         return
 
 if __name__ == "__main__":
     s = swapper()
+    s.run()    
--- src/twitter.py
+++ src/twitter.py
@@ -7,42 +7,91 @@ from BeautifulSoup import BeautifulSoup
 from HTMLParser import HTMLParser
 TWITTERUSER = "splatoonmaps"
 
+def getAll():
+    # get the most recent tweet from Twitter
+    page = urllib2.urlopen("http://twitter.com/" + TWITTERUSER)
+    soup = BeautifulSoup(page)
 
-# get the most recent tweet from Twitter
-page = urllib2.urlopen("http://twitter.com/" + TWITTERUSER)
-soup = BeautifulSoup(page)
+    #print soup#.getText()
 
-#print soup#.getText()
+    contentTags = soup.findAll('p', attrs={"lang" : "en" })
+    twitterTweet = contentTags[0]
 
-contentTags = soup.findAll('p', attrs={"lang" : "en" })
-twitterTweet = contentTags[0]
+    #print contentTags
 
-#print contentTags
-
-sep = '#'
+    sep = '#'
 
 
-for i in contentTags:
-    info = i.getText()
-    info = info.replace("&amp;", "&")
-    info = info.split(sep, 1)[0]
+    for i in contentTags:
+        info = i.getText()
+        info = info.replace("&amp;", "&")
+        info = info.split(sep, 1)[0]
 
-    lobby = info.split("maps", 1)[0]
-    rest = info.split("maps", 1)[1]
-    game = rest.split(":",1)[0]
-    game = game.replace(game[:2], '')
-    mapA = info.split("& ", 1)[0]  
-    mapA = mapA.split(": ", 1)[1]  
-    mapB = info.split("& ", 1)[1]
+        lobby = info.split("maps", 1)[0]
+        rest = info.split("maps", 1)[1]
+        game = rest.split(":",1)[0]
+        game = game.replace(game[:2], '')
+        mapA = info.split("& ", 1)[0]  
+        mapA = mapA.split(": ", 1)[1]  
+        mapB = info.split("& ", 1)[1]
 
-    #stripppp
-    lobby = lobby.strip()
-    game = game.strip()
-    mapA = mapA.strip()
-    mapB = mapB.strip()
-    print "Lobby     :", lobby
-    if "Turf" not in lobby: 
-        print "Game Mode :", game
-    print "Map A     :", mapA
-    print "Map B     :", mapB
-    print '----------------------------------'
+        #stripppp
+        lobby = lobby.strip()
+        game = game.strip()
+        mapA = mapA.strip()
+        mapB = mapB.strip()
+        print "Lobby     :", lobby
+        if "Turf" not in lobby: 
+            print "Game Mode :", game
+        print "Map A     :", mapA
+        print "Map B     :", mapB
+        print '----------------------------------'
+
+def getRecent():
+    # get the most recent tweet from Twitter
+    page = urllib2.urlopen("http://twitter.com/" + TWITTERUSER)
+    soup = BeautifulSoup(page)
+
+    #print soup#.getText()
+
+    contentTags = soup.findAll('p', attrs={"lang" : "en" })
+    twitterTweet = contentTags[0]
+
+    #print contentTags
+
+    sep = '#'
+
+    #For some reason, it's being inconsistent for
+    #Turf Wars...
+    for i in range(0,1):
+        info = contentTags[i].getText()
+        info = info.replace("&amp;", "&")
+        info = info.split(sep, 1)[0]
+
+        lobby = info.split("maps", 1)[0]
+        rest = info.split("maps", 1)[1]
+        game = rest.split(":",1)[0]
+        game = game.replace(game[:2], '')
+        mapA = info.split("& ", 1)[0]  
+        mapA = mapA.split(": ", 1)[1]  
+        mapB = info.split("& ", 1)[1]
+
+        #stripppp
+        lobby = lobby.strip()
+        game = game.strip()
+        mapA = mapA.strip()
+        mapB = mapB.strip()
+        #print "Lobby     :", lobby
+        #if "Turf" not in lobby: 
+        #    print "Game Mode :", game
+        #print "Map A     :", mapA
+        #print "Map B     :", mapB
+        #print '----------------------------------'
+    return lobby, game, mapA, mapB
+
+if __name__ == "__main__":
+    
+    print "ALL"
+    getAll()
+    print "RECENT"
+    getRecent()
